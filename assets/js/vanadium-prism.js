914bbdc9140db3de32f2d892b71dbff5db92c1c8
Nykenik24 <Nykenik24@users.noreply.github.com>
2025-05-10 13:30:54 +0000

    deploy: 704b20339b8277f21ef492ceb9d14cd83cb4fd22

--- assets/js/vanadium-prism.js
+++ assets/js/vanadium-prism.js
@@ -1,20 +1,36 @@
 Prism.languages.vanadium = {
-  keyword: /\b(fn|if|else|return|mut|imut|include|publ|priv|shared|frozen)\b/,
-  type: /\b(int16|int32|int64|uint8|uint16|uint32|uint64|float16|float32|float64|type|void|bool|str)\b/,
+  comment: {
+    pattern: /@.*|\/\*[\s\S]*?(?:\*\/|$)/,
+    greedy: true,
+  },
+
+  keyword:
+    /\b(fn|if|else|return|for|while|include|unsafe|struct|new|mut|imut)\b/,
   boolean: /\b(?:false|true)\b/,
 
-  number: /\b\d+(\.\d+)?\b/,
+  weak_keyword: {
+    pattern: /\b(in|as|frozen|publ|priv|shared)\b/,
+    alias: "boolean",
+  },
+
+  number: [/\b\d+(\.\d+)?\b/, /\b0x[0-9a-fA-F]+\b/, /\b0b[01]+\b/],
 
   string: /"(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*'/,
 
   function: {
     pattern: /\b\w+(?=\()/,
-    alias: "variable",
+    alias: "property",
   },
+
+  type: {
+    pattern:
+      /\b(int16|int32|int64|uint8|uint16|uint32|uint64|float16|float32|float64|type|void|bool|str)\b/,
+    alias: "property",
+  },
+
+  special: { pattern: /\b(std|io|self)\b/, alias: "property" },
 
   operator: /[-+*/%=!<>]=?/,
 
-  comment: /@.*/,
-
-  variable: /\b[a-zA-Z_][a-zA-Z0-9_]*\b/,
+  variable: [/\b[a-zA-Z_][a-zA-Z0-9_]*\b/],
 };
